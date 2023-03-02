337c57ba861b20ed1a238ba5b6acc6a7696d14b4
Clark Saben <76020733+csaben@users.noreply.github.com>
2023-03-02 14:46:56 -0500

    Add files via upload
    you still need to re-instantiate

--- State.py
+++ State.py
@@ -10,6 +10,7 @@ import json
 from pathlib import Path
 import PlannerProto_pb2
 import numpy as np
+import time
 
 class State(AiManager):
     # Constructor
@@ -21,6 +22,7 @@ class State(AiManager):
         self.directory = Path("./output2/")
         self.base_file = "ttd_state.json"
         self.filepath = self.get_next_filepath(self.directory, self.base_file)
+        self.ifYouShootShutUp=[]
 
 
    
@@ -71,10 +73,13 @@ class State(AiManager):
             output_message: OutputPb = OutputPb()
 
             execution_order = [enemy for enemy in execution_order if enemy[1] not in self.memory]
-            print(execution_order)
+            # print(execution_order)
             #make sure its still sorted (it should be lol)
             execution_order = sorted(execution_order, key=lambda x: x[0])
 
+            if json_dict.get("time") > 130 and json_dict.get("time") <135:
+                self.memory=[]
+                self.ifYouShootShutUp=[]
             for missle in execution_order:
                 ship_action: ShipActionPb = ShipActionPb()
                 #set the target id to the missle id
@@ -82,39 +87,83 @@ class State(AiManager):
                 ship_action.weapon, ship_action.AssetName \
                         = self.whoShootsFirst(json_dict['assets'], missle[1])
 
-                output_message.actions.append(ship_action)
+                if ship_action.weapon != "":
+                    output_message.actions.append(ship_action)
+
+                else:
+                    self.ifYouShootShutUp=[]
+                    return output_message
+            print("printing execution order")
+            print(execution_order)
+            print("printing memory")
+            print(self.memory)
+            self.ifYouShootShutUp=[]
+            print("printing output message")
+            print(output_message.actions )
             return output_message
 
+    def find_value(self, list_of_dicts, key1, value1, key2, target_id):
+        """
+        Find a value in a list of dictionaries that contain another list of dictionaries.
+
+        Args:
+        - list_of_dicts: a list of dictionaries that contain another list of dictionaries.
+        - key1: the key to search for in the first level of dictionaries.
+        - value1: the value to search for in the first level of dictionaries.
+        - key2: the key to search for in the second level of dictionaries.
+
+        Returns:
+        - The value associated with the key2 in the first matching dictionary.
+        - None if the value1 is not found in any of the dictionaries.
+        """
+        whosShooting=""
+        withWhat=""
+        #update after you send a message each time
+        # ifYouShootShutUp=[]
+        for dictionary in list_of_dicts:
+            # if dictionary.get(key1) != value1:
+            if dictionary.get(key1) != value1:# and dictionary.get("AssetName") not in self.ifYouShootShutUp:
+                # print(self.ifYouShootShutUp)
+                whosShooting=dictionary.get("AssetName")
+                for nested_dict in dictionary.get("weapons"):
+                    if key2 in nested_dict.keys() and (dictionary.get("AssetName"), nested_dict.get("SystemName")) not in self.ifYouShootShutUp:
+                        withWhat=nested_dict.get("SystemName")
+                        self.memory.append(target_id)
+                        self.ifYouShootShutUp.append((whosShooting,withWhat))
+                        return withWhat, whosShooting
+        return "", ""
 
     def whoShootsFirst(self, assets, target_id)-> tuple:
-        '''
-        (
-        ship_action.AssetName = "Galleon HVU"
-        ship_action.weapon = "Chainshot_System"
-        )
-        '''
-        for asset in assets:
-            if asset['health'] == -1:
-                pass
-            else:
-                for weapon in asset['weapons']:
-                    print(weapon)
-                    #BUG: quantity sometimes not given by the planner
-                    if weapon['WeaponState'] == 'Ready' and "Quantity" in weapon.keys():
-
-                        print(target_id)
-                        self.memory.append(target_id)
-                        print("memory: ", self.memory)
-                        # print(weapon['SystemName'], asset['AssetName'])
-                        # with open("./issue1.json", 'a') as f:
-                        #     f.write(str(target_id))
-                        #     f.write(str(self.memory))
-                        # import sys
-                        # sys.exit()
-
-                        return  weapon['SystemName'], asset['AssetName']
-                    else:
-                        pass
+        #start over because its not working
+        return self.find_value(assets, "health", -1, "Quantity", target_id)
+        # '''
+        # (
+        # ship_action.AssetName = "Galleon HVU"
+        # ship_action.weapon = "Chainshot_System"
+        # )
+        # '''
+        # # print(assets)
+        # whosShooting = None
+        # for asset in assets:
+        #     if asset['health'] == -1:
+        #         pass
+        #     else:
+        #         time.sleep(.5)
+        #         for weapon in asset['weapons']:
+        #             if (weapon['WeaponState'] == 'Ready') and ("Quantity" in weapon.keys()) and (asset['AssetName']!="HVU_Galleon_0"):
+        #                 print(asset["AssetName"])
+        #                 if weapon["Quantity"] >3:
+        #                     # print(target_id)
+        #                     self.memory.append(target_id)
+        #                     print("memory: ", self.memory)
+        #
+        # # return "Cannon_System", "HVU_Galleon_0"
+        #                     return  weapon['SystemName'], asset['AssetName']
+        #                 else:
+        #                     pass
+        #
+        #             else:
+        #                 pass
 
 
     @staticmethod
@@ -124,20 +173,25 @@ class State(AiManager):
         #get position, divide by speed, sort by lowest time to intercept
         #exclude elevation for now
         for missle in missle_list:
-            #easier to read
-            x = missle['PositionX']
-            y = missle['PositionY']
-            vx = missle['VelocityX']
-            vy = missle['VelocityY']
+            if ">" in missle.get("ThreatId"):
+                pass
+            else:
 
-            #distance eqn
-            d = np.sqrt((x-origin[0])**2 + (y-origin[1])**2)
-            #velocity eqn
-            dv = np.sqrt(vx**2 + vy**2)
+                #easier to read
+                x = missle['PositionX']
+                y = missle['PositionY']
+                vx = missle['VelocityX']
+                vy = missle['VelocityY']
 
-            #time to intercept
-            t = d/dv
-            intercept_times.append((t, missle['TrackId']))
+                #distance eqn
+                d = np.sqrt((x-origin[0])**2 + (y-origin[1])**2)
+                #velocity eqn
+                dv = np.sqrt(vx**2 + vy**2)
+
+                #time to intercept
+                t = d/dv
+                print(missle)
+                intercept_times.append((t, missle['TrackId']))
 
         return sorted(intercept_times, key=lambda x: x[0])
 
@@ -146,21 +200,15 @@ class State(AiManager):
         gamma = 1e-6
         origin = np.array([0,0], dtype=float)
         for asset in asset_list:
-            if asset['health'] ==-1:
-                pass
+            if 'health' in asset.keys():
+                if asset['health'] == -1:
+                    pass
+                else:
+                    origin+= np.array([asset['PositionX'], asset['PositionY']])
             else:
-                # print(asset)
-                origin+= np.array([asset['PositionX'], asset['PositionY']])
+                #when no healths just return zero (some bug)
+                return 0
         return origin/(len(asset_list)-1 + gamma)
-
-
-
-
-
-
-
-
-
 
 
     def cleanState(self, msg:StatePb):
